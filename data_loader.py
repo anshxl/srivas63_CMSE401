da059db0d886ff530ecdce838fdafb971246c61d
Anshul Srivastava <AnshulSrivastava@Anshuls-MacBook-Air-2.local>
2025-02-20 20:40:26 -0500

    Adjusted one-hot function to only use NumPy

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,11 @@
 #Load MNIST data
 import tensorflow.keras.datasets # type: ignore
+import numpy as np
+
+def one_hot_encode(y):
+    one_hot = np.zeros((y.size, y.max() + 1))
+    one_hot[np.arange(y.size), y] = 1
+    return one_hot
 
 def load_data():
     mnist = tensorflow.keras.datasets.mnist.load_data(
@@ -12,8 +18,8 @@ def load_data():
     #Normalize pixel values to between 0 and 1
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    #One-hot encode the labels
-    y_train = tensorflow.keras.utils.to_categorical(y_train, 10)
-    y_test = tensorflow.keras.utils.to_categorical(y_test, 10)
+    #One-hot encode the labels using NumPy
+    y_train = one_hot_encode(y_train)
+    y_test = one_hot_encode(y_test)
 
     return (x_train, y_train), (x_test, y_test)
